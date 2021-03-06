5b23b19a8f66671bb3fee8caf584542d7c024ca3
Jat Torres <jat.torres@gmail.com>
2021-03-07 00:36:17 +0800

    fixed dialog form

--- src/CompanyDetail.tsx
+++ src/CompanyDetail.tsx
@@ -3,17 +3,21 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle,
-  Drawer,
+  InputLabel,
+  MenuItem,
+  Select,
   TextField,
 } from "@material-ui/core";
 import * as React from "react";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
 import {
   CompanySearchResponse,
   CompanySponsorDetailResponse,
+  JobApplicationCreateDto,
+  JobApplicationCreateDtoApplicationMethodEnum,
+  JobApplicationCreateDtoStatusEnum,
+  JobApplicationCreateDtoTechCompanyTypeEnum,
 } from "./api/generated";
 import SponsorshipApi from "./api/SponsorshipApi";
 
@@ -29,6 +33,10 @@ const CompanyDetail: React.FC<{}> = (props) => {
     ukTierSponsor,
     setUKTierSponsor,
   ] = React.useState<CompanySearchResponse>();
+  const [
+    jobApplication,
+    setJobApplication,
+  ] = React.useState<JobApplicationCreateDto>();
   const [isDialogOpen, setDrawerOpen] = React.useState<boolean>(false);
   const { id } = useParams<CompanyDetailRequestParams>();
 
@@ -44,8 +52,29 @@ const CompanyDetail: React.FC<{}> = (props) => {
     });
   }, []);
 
+  const saveJobApplicationEvent = () => {
+    if (jobApplication) {
+      SponsorshipApi.postJobApplicationEvent(jobApplication);
+    }
+  };
+
   const handleOpenDialog = () => setDrawerOpen(true);
   const handleCloseDialog = () => setDrawerOpen(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<{
+      name?: string | undefined;
+      value: unknown;
+    }>,
+    s: any
+  ) => {
+    const { name, value } = e.currentTarget;
+    if (name) {
+      setJobApplication((oldValue) => {
+        return { ...oldValue, [name]: value };
+      });
+    }
+  };
 
   return (
     <div>
@@ -77,16 +106,68 @@ const CompanyDetail: React.FC<{}> = (props) => {
         onClose={handleCloseDialog}
         aria-labelledby="form-dialog-title"
       >
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogTitle id="form-dialog-title">Job Application</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
+          <InputLabel id="jobapplication-applicationmethod-label">
+            Application method
+          </InputLabel>
+          <Select
+            labelId="jobapplication-applicationmethod-label"
+            id="jobapplication-applicationmethod-select"
+            name="applicationMethod"
+            value={jobApplication?.applicationMethod}
+            onChange={handleChange}
+          >
+            {Object.values(JobApplicationCreateDtoApplicationMethodEnum).map(
+              (method) => (
+                <MenuItem value={method}>{method}</MenuItem>
+              )
+            )}
+          </Select>
+          <InputLabel id="jobapplication-status-label">
+            Application status
+          </InputLabel>
+          <Select
+            labelId="jobapplication-status-label"
+            id="jobapplication-status-select"
+            name="status"
+            value={jobApplication?.status}
+            onChange={handleChange}
+          >
+            {Object.values(JobApplicationCreateDtoStatusEnum).map((status) => (
+              <MenuItem value={status}>{status}</MenuItem>
+            ))}
+          </Select>
+          <InputLabel id="jobapplication-category-label">
+            Tech Company Category
+          </InputLabel>
+          <Select
+            labelId="jobapplication-category-label"
+            id="jobapplication-category-select"
+            name="techCompanyType"
+            value={jobApplication?.techCompanyType}
+            onChange={handleChange}
+          >
+            {Object.values(JobApplicationCreateDtoTechCompanyTypeEnum).map(
+              (status) => (
+                <MenuItem value={status}>{status}</MenuItem>
+              )
+            )}
+          </Select>
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="website"
+            name="website"
+            label="Website"
+            type="text"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="email"
+            name="email"
             label="Email Address"
             type="email"
             fullWidth
